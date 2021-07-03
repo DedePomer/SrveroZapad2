073e5dc0251a781f7335d1e23684bdb4ae54a622
Saha <t343st@gmail.com>
2021-07-03 09:58:16 +0300

    Аоявился Класс

--- Class1.cs
+++ Class1.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace Зкз2021
 {
-    class Class1
-    {
-    }
+    
+     public class Class1
+     {
+        public int[,] Severo_Zapad(int[] VectorM, int[] VectorN, int M, int N, int[,] MatrixB) // Сам метод
+        {
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (VectorM[i] == VectorN[j] && VectorM[i] != 0 && VectorN[j] != 0)
+                    {
+                        MatrixB[i, j] = VectorM[i];
+                        VectorM[i] = 0;
+                        VectorN[j] = 0;
+                    }
+                    else if (VectorM[i] < VectorN[j] && VectorM[i] != 0 && VectorN[j] != 0)
+                    {
+                        MatrixB[i, j] = VectorM[i];
+                        VectorN[j] -= VectorM[i];
+                        VectorM[i] = 0;
+                    }
+                    else if (VectorM[i] > VectorN[j] && VectorM[i] != 0 && VectorN[j] != 0)
+                    {
+                        MatrixB[i, j] = VectorN[j];
+                        VectorM[i] -= VectorN[j];
+                        VectorN[j] = 0;
+                    }
+                }
+            }
+            return MatrixB;
+        }
+        public int[] ZapolnVectora(int[] vector, string s)
+        {
+            string[] Smas = s.Split(' ');
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = Convert.ToInt32(Smas[i]);
+            }
+            return vector;
+        }
+     }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Diagnostics;
 using Microsoft.VisualBasic.FileIO;
 
-namespace Северо_запад
+namespace Зкз2021
 {
     class Program
     {
@@ -12,10 +12,18 @@ namespace Северо_запад
             string[] C = File.ReadAllLines(path);           
             return C;
         }
+        static void InfoVectora(int[] vector)// метод выводящий отладочные данные
+        {
+            Debug.WriteLine("Вектор " + vector);
+            foreach (var n in vector)
+            {
+                Debug.WriteLine(n);
+            }
+        }
         static void Main(string[] args)
         {
-            string path = "New.csv";
-            //Class1 SZ = new Class1();  
+            string path = "New.csv"; 
+            Class1 SZ = new Class1(); 
             string[] SuperData = File.ReadAllLines(path);
             string[] Data = Chtenie(path); 
             foreach (var item in Data)
@@ -43,10 +51,34 @@ namespace Северо_запад
                     if (Convert.ToInt32(Nlenght[j]) == 0)
                         k++;
                 }
-
             }
             if (razN == razVN && razVM == (Data.Length - 2) && sumM == sumN && k == 0)
-            { }
+            {
+                Nlenght = Data[2].Split(' ');
+                int M, N;
+                M = Data.Length - 2;
+                N = Nlenght.Length;
+
+                Debug.WriteLine("Значение N = " + N + " Значение M = " + M);
+                int[] VectorN = new int[N]; //Создание векторов
+                int[] VectorM = new int[M];
+
+                VectorN = SZ.ZapolnVectora(VectorN, Data[0]);//Заполнение векторов
+                VectorM = SZ.ZapolnVectora(VectorM, Data[1]);
+                InfoVectora(VectorN);
+                InfoVectora(VectorM);
+
+                int[,] MatrixA = new int[M, N];
+                int[,] MatrixB = new int[M, N];
+                for (int i = 0; i < M; i++)// Заполнение матрицы
+                {
+                    Nlenght = Data[i + 2].Split(' ');
+                    for (int j = 0; j < N; j++)
+                    {
+                        MatrixA[i, j] = Convert.ToInt32(Nlenght[j]);
+                    }
+                }
+            }
             
             
         }
